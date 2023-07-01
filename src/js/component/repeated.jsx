42c8837bd8bafee5cb9afe364b64d322b300dd86
Cody Ross <130109458+Rossco1991@users.noreply.github.com>
2023-07-01 01:01:19 +0000

    In process of working on project. Repeated function not working properly

---
+++ src/js/component/repeated.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const Repeated = () => {
+  return (
+    <div className="card" style="width: 18rem;">
+     /* <img src="https://via.placeholder.com/500x325" alt="" />        *
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <a href="#" className="btn btn-primary">
+          Find Out More
+        </a>
+    </div>
+  )
+}
