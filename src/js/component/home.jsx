e87cae676ced663c688e06257404baa44fa8b2c3
Cody Ross <130109458+Rossco1991@users.noreply.github.com>
2023-07-06 00:13:32 +0000

    Still working on issue with cards populating correctly

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,13 +5,25 @@ import { Repeated } from "./repeated";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+let cardInfo = [
+  {
+    image: "https://miro.medium.com/v2/resize:fit:500/0*-ouKIOsDCzVCTjK-.png",
+    title: "Card Title",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    button: "Find out More!",
+  }
+]
+
+
 //create your first component
 const Home = () => {
   return (
     <>
       <Navbar />
       <Jumbotron />
-      <Repeated />
+      <div className="container">
+        <Repeated data={cardInfo.map(cardInfo, idx)}/>
+      </div>
     </>
   );
 };
