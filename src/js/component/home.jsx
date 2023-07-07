f4b0e334d5a4253e778bcdddf096e78d0ea853d9
Cody Ross <130109458+Rossco1991@users.noreply.github.com>
2023-07-07 22:54:35 +0000

    Put in prop types to repeated

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,14 +5,6 @@ import { Repeated } from "./repeated";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-let cardInfo = [
-  {
-    image: "https://miro.medium.com/v2/resize:fit:500/0*-ouKIOsDCzVCTjK-.png",
-    title: "Card Title",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    button: "Find out More!",
-  }
-]
 
 
 //create your first component
@@ -22,7 +14,7 @@ const Home = () => {
       <Navbar />
       <Jumbotron />
       <div className="container">
-        <Repeated data={cardInfo.map(cardInfo, idx)}/>
+        <Repeated imageUrl="" title="" description="" buttonLabel=""/>
       </div>
     </>
   );
--- src/js/component/repeated.jsx
+++ src/js/component/repeated.jsx
@@ -1,19 +1,28 @@
 import React, { useState } from "react";
+import PropTypes from 'prop-types';
 
 
 
-export const Repeated = (data=[]) => {
+export const Repeated = props => {
 
   return (
       <div className="card text-center h-100">
-        <img src={data.image} key={idx} className="card-img-top" />
+        <img src={props.imageUrl} className="card-img-top" />
         <div className="card-body">
-          <h5 className="card-title">{data.title}</h5>
-          <p className="card-text">{data.description}</p>
+          <h5 className="card-title">{props.title}</h5>
+          <p className="card-text">{props.description}</p>
         </div>
         <div className="card-footer bg-white">
-          <a href="#" className="btn btn-primary">{data.button}</a>
+          <a href="#" className="btn btn-primary">{props.button}</a>
         </div>
       </div>
   )
+}
+
+Repeated.propTypes = {
+  imageUrl: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  buttonLabel: PropTypes.string,
+
 }
