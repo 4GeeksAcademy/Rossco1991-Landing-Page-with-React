495b1b0d81a1c48b36606337254dfdf627213ca1
Cody Ross <130109458+Rossco1991@users.noreply.github.com>
2023-07-16 00:22:30 +0000

    Able to complete project

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export const Footer = () => {
+    
+    return(
+        <div className="bg-dark text-center">
+            Created by Cody Ross - 2023
+        </div>
+    )
+}
--- src/js/component/repeated.jsx
+++ src/js/component/repeated.jsx
@@ -1,28 +1,20 @@
 import React, { useState } from "react";
-import PropTypes from 'prop-types';
 
 
+export const Card= (props) => {
 
-export const Repeated = props => {
+  
 
   return (
-      <div className="card text-center h-100">
-        <img src={props.imageUrl} className="card-img-top" />
+      <div className="card text-center mx-3">
+        <img src={props.img} className="card-img-top" />
         <div className="card-body">
           <h5 className="card-title">{props.title}</h5>
-          <p className="card-text">{props.description}</p>
+          <p className="card-text">{props.text}</p>
         </div>
         <div className="card-footer bg-white">
           <a href="#" className="btn btn-primary">{props.button}</a>
         </div>
       </div>
   )
-}
-
-Repeated.propTypes = {
-  imageUrl: PropTypes.string,
-  title: PropTypes.string,
-  description: PropTypes.string,
-  buttonLabel: PropTypes.string,
-
 }
